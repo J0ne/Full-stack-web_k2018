3df844fc62ad7aa26cb09ba2d003b493e72e764c
Jouni Männistö <jouni.mannisto@gmail.com>
2018-01-21 15:43:34 +0200

    2.6 puhelinluettelo osa 1

--- Palautukset/Osa 2/part2-notes-part2-3/src/App.js
+++ Palautukset/Osa 2/part2-notes-part2-3/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import Note from './components/Note'
+import Person from './components/Person'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
+    console.log(props.persons)
     this.state = {
-      notes: props.notes,
-      newNote: '',
+      persons: props.persons,
+      newPerson: '',
       showAll: true
     }
   }
@@ -15,54 +16,57 @@ class App extends React.Component {
     this.setState({ showAll: !this.state.showAll })
   }
 
-  addNote = (event) => {
+  addPerson = (event) => {
     event.preventDefault()
-    const noteObject = {
-      content: this.state.newNote,
-      date: new Date().new,
-      important: Math.random() > 0.5,
-      id: this.state.notes.length + 1
+    const personObj = {
+      name: this.state.newPerson,
+      // date: new Date().new,
+      // important: Math.random() > 0.5,
+      id: this.state.persons.length + 1
     }
 
-    const notes = this.state.notes.concat(noteObject)
+    const persons = this.state.persons.concat(personObj)
 
     this.setState({
-      notes,
-      newNote: ''
+      persons,
+      newPerson: ''
     })
+    console.log('Hlö lisätty: ', personObj, persons)
   }
 
   handleNoteChange = (event) => {
     console.log(event.target.value)
-    this.setState({ newNote: event.target.value })
+    this.setState({ newPerson: event.target.value })
   }
 
   render() {
-    const notesToShow =
-      this.state.showAll ?
-        this.state.notes :
-        this.state.notes.filter(note => note.important === true)
+    const personsToShow = this.state.persons
+      // this.state.showAll ?
+      //   this.state.persons :
+      //   this.state.persons.filter(note => true)
 
     const label = this.state.showAll ? 'vain tärkeät' : 'kaikki'
 
     return (
       <div>
-        <h1>Muistiinpanot</h1>
-        <div>
-          <button onClick={this.toggleVisible}>
-            näytä {label}
-          </button>
-        </div>
-        <ul>
-          {notesToShow.map(note => <Note key={note.id} note={note} />)}
-        </ul>
-        <form onSubmit={this.addNote}>
+        <h1>Puhelinluettelo</h1>
+         <form onSubmit={this.addPerson}>
           <input 
-            value={this.state.newNote} 
+            value={this.state.newPerson} 
             onChange={this.handleNoteChange}
           />
-          <button type="submit">tallenna</button>
+          <button type="submit">lisää</button>
         </form>
+        <div>
+          {/* <button onClick={this.toggleVisible}>
+            näytä {label}
+          </button> */}
+        </div>
+        <h2>Numerot</h2>
+        <ul>
+          {personsToShow.map(person => <Person key={person.id} name={person.name} />)}
+        </ul>
+       
       </div>
     )
   }
