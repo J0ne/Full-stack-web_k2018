7bcce3ca6b8c805756414e37c1239adfa9d06e08
Jouni Männistö <jouni.mannisto@gmail.com>
2018-02-04 18:31:34 +0200

    Osan 4 alustus

---
+++ Palautukset/Osa 4/part3-notes-backend-part4-1/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const notesRouter = require('./controllers/notes')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+mongoose.Promise = global.Promise
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static('build'))
+app.use(middleware.logger)
+
+app.use('/api/notes', notesRouter)
+
+app.use(middleware.error)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
