1832909cc7ba2bc79e96fb936a0e2e1a84147c48
Jouni Männistö <jouni.mannisto@gmail.com>
2018-02-27 18:42:04 +0200

    5.6* blogilistan frontend, osa 6

--- Palautukset/Osa 5/bloglist-frontend-master/src/components/Blog.js
+++ Palautukset/Osa 5/bloglist-frontend-master/src/components/Blog.js
@@ -1,8 +1,56 @@
 import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+
+const showUser = (blog) => {
+  if(blog.postedBy != null){
+    return 'added by ' + blog.postedBy.name;
+  }
+}
+
+class Blog extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      blog: props.blog,
+      showDetails: false
+    }
+  }
+
+  toggleDetails = () => {
+    console.log("Toimii")
+    console.log(this.state)
+    this.setState({ showDetails: !this.state.showDetails})
+  }
+
+  render(){
+
+    const blogStyle = {
+      paddingTop: 5,
+      paddingBottom: 5,
+      paddingLeft: 5,
+      border: '1px solid black',
+      borderWidth: 1,
+      marginBottom: 5,
+      borderRadius: '4px'
+    }
+    const titleStyle = {
+      cursor: 'pointer',
+      fontWeight: 'bolder'
+    }
+    //const show = { display: this.state.showDetails ? 'none' : '' }
+    const hide = { display: this.state.showDetails ? '' : 'none' }
+    return (
+      <div style={blogStyle}>
+        <div>
+          <span style={titleStyle} onClick={this.toggleDetails}>{this.state.blog.title}</span> {this.state.blog.author}
+        </div>
+        <div style={hide}>
+          <p><a href={this.state.blog.url}>{this.state.blog.url}</a></p>
+          {this.state.blog.likes} <button>like</button> <br />
+          {showUser(this.state.blog)}
+        </div>
+      </div>
+    )
+  }
+} 
 
 export default Blog
