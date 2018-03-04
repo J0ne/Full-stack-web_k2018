5a83657807f5c57045504eb13e5d3f8a10249859
Jouni Männistö <jouni.mannisto@gmail.com>
2018-03-04 12:41:20 +0200

    6.2 paremmat anekdootit, osa 1 Chrome herjaa tästä: export const actionFor =  {...    <- unexpected token export
    mutta sovellus toimii. eli liekö kyseessä nyt syntaksin standardoimattomuus

--- Palautukset/Osa 6/redux-anecdotes-v2-master/src/components/AnecdoteForm.js
+++ Palautukset/Osa 6/redux-anecdotes-v2-master/src/components/AnecdoteForm.js
@@ -1,14 +1,14 @@
 import React from 'react'
+import { actionFor } from '../reducers/anecdoteReducer'
 
 class AnecdoteForm extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault()
-    const content = e.target.anecdote.value
-    this.props.store.dispatch({ 
-      type: 'CREATE', 
-      content 
-    })
-  
+    const anecdote = e.target.anecdote.value
+    console.log(anecdote)
+     this.props.store.dispatch(
+       actionFor.anecdoteCreation(anecdote)
+    )  
     e.target.anecdote.value = ''
   }
    render() {
--- Palautukset/Osa 6/redux-anecdotes-v2-master/src/reducers/anecdoteReducer.js
+++ Palautukset/Osa 6/redux-anecdotes-v2-master/src/reducers/anecdoteReducer.js
@@ -20,6 +20,7 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (store = initialState, action) => {
+  console.log(store, action)
   if (action.type==='VOTE') {
     const old = store.filter(a => a.id !==action.id)
     const voted = store.find(a => a.id === action.id)
@@ -27,11 +28,20 @@ const reducer = (store = initialState, action) => {
     return [...old, { ...voted, votes: voted.votes+1} ]
   }
   if (action.type === 'CREATE') {
-
     return [...store, { content: action.content, id: getId(), votes:0 }]
   }
 
   return store
 }
+export const actionFor =  {
+    anecdoteCreation(content){
+    console.log(content)
+    return {
+      type: 'CREATE',
+      content
+    }
+  }
+}
+
 
 export default reducer
