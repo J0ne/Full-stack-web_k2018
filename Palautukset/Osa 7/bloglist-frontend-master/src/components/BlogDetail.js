56f2e8f83f6857d8f07db9792d380fa1e0f3b8bc
Jouni Männistö <jouni.mannisto@gmail.com>
2018-03-10 23:08:26 +0200

    7.4 blogin näkymä Nyt myös like päivittyy UI:ssa.

--- Palautukset/Osa 7/bloglist-frontend-master/src/components/BlogDetail.js
+++ Palautukset/Osa 7/bloglist-frontend-master/src/components/BlogDetail.js
@@ -3,38 +3,46 @@ import { connect } from 'react-redux'
 import blogService from '../services/blogs'
 import { notify } from '../reducers/notificationReducer'
 // import { userInitialization } from '../reducers/userReducer'
-const addlike = (blog) => {
-
-    blog.likes++
-    const response = blogService.addLike(blog).then(response => {
-        // this.setState({ blog: response })
-        console.log(response)
-        //this.showInfo(`${blogData.title} liked!`, "info")
-    }).catch(err => console.log(err))
-    //this.props.notify(`${blog.title} liked! `, 1)
-}
-const showBlog = (blog) => {
-    if(blog){
-        return (<div>
-            <h2>{blog != null ? blog.title : ''}</h2>
-            <div className="" >
-                <p><a href={blog.url}>{blog.url}</a></p>
-                <p>{blog.likes} likes</p>
-                <button className="btn-like" onClick={addlike(blog)}>like</button> <br />
-                {/* {showUser(this.state.blog)} */}
-                {/* {this.showDeleteButton(this.state.blog)} */}
-            </div>
-        </div>)
-    }else{
-        return (<div>...</div>
-)    }
-}
 
 class BlogDetail extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            blog: props.blog
+        }
+    }
+    showBlog(blog){
+        if (blog) {
+            return (<div>
+                <h2>{blog != null ? blog.title : ''}</h2>
+                <div className="" >
+                    <p><a href={blog.url}>{blog.url}</a></p>
+                    <p>{blog.likes} likes</p>
+                    <button className="btn-like" onClick={() => this.addlike(blog)}>like</button> <br />
+                    {/* {showUser(this.state.blog)} */}
+                    {/* {this.showDeleteButton(this.state.blog)} */}
+                </div>
+            </div>)
+        } else {
+            return (<div>...</div>
+            )
+        }
+    }
+
+    addlike(blog) {
+
+        blog.likes++
+        const response = blogService.addLike(blog).then(response => {
+            this.setState({ blog: response })
+            console.log(response)
+            //this.showInfo(`${blogData.title} liked!`, "info")
+        }).catch(err => console.log(err))
+        //this.props.notify(`${blog.title} liked! `, 1)
+    }
 
     render() {
         const { blog, handleLike } = this.props
-        return showBlog(blog)
+        return this.showBlog(blog)
     }
 
 }
