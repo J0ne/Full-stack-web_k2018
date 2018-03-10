1f8bf53101f65beb5127f041fc53dc2a7e9951d8
Jouni Männistö <jouni.mannisto@gmail.com>
2018-03-10 13:01:02 +0200

    7.8 redux, osa 1 toinen commit, puuttuvat filet

---
+++ Palautukset/Osa 7/bloglist-frontend-master/src/store.js
@@ -0,0 +1,17 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+// import anecdoteReducer from './reducers/anecdoteReducer'
+import notificationReducer from './reducers/notificationReducer'
+// import filterReducer from './reducers/filterReducer'
+// import { composeWithDevTools } from 'redux-devtools-extension'
+const reducer = combineReducers({
+    notification: notificationReducer
+    // ,
+    // filter: filterReducer
+})
+const store = createStore(
+    reducer,
+    applyMiddleware(thunk)
+)
+
+export default store
